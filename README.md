5dcd8b5cb4ab3ad33f75cd4b58e8a0f10a0143c2
Lin.Yao <linyao@foxmail.com>
2017-09-26 15:37:58 +0800

    v1.0
    抓取PDF并保存至本地

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # rmfyb-pdf
+
+人民法院报 [每周四第六版][1]
+         [每周四第七版][2]
+
+邮件发送至指定邮箱
+
+[1]:	http://rmfyb.chinacourt.org/paper/images/2017-09/21/06/2017092106_pdf.pdf
+[2]:	http://rmfyb.chinacourt.org/paper/images/2017-09/21/07/2017092107_pdf.pdf
---
+++ catch.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 14:28:35 2017
+
+@author: Leon
+"""
+
+from datetime import datetime
+import urllib
+
+def catch():
+     #分解now，并生成PDF链接
+    Ym = now.strftime('/%Y-%m/')
+    d = now.strftime('%d/')
+    Ymd = now.strftime('%Y%m%d')
+    Ymd06 = now.strftime('%Y%m%d''06_pdf.pdf')
+    Link06 = 'http://rmfyb.chinacourt.org/paper/images' + Ym + d + '06/' +Ymd06 #第六版链接
+    Ymd07 = now.strftime('%Y%m%d''07_pdf.pdf')
+    Link07 = 'http://rmfyb.chinacourt.org/paper/images' + Ym + d + '07/' +Ymd07 #第七版链接
+    print Link06,Link07
+    #保存本地
+    urllib.urlretrieve(Link06, Ymd + '06.pdf')
+    urllib.urlretrieve(Link07,Ymd + '07.pdf')
+
+print "抓取程序开始运行..."
+now = datetime.now() # 获取当前时间
+baseday = datetime.strptime('2010-12-30', '%Y-%m-%d') #周四基准日
+
+
+if (now - baseday).days %7 == 0: #判断是否为周四，如果是则执行下一步
+    print('今天是周四，抓取开始') 
+    catch()
