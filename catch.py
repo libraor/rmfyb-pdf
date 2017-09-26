0283b350eb9602c7049c38874ee30f569a431e5d
Lin.Yao <linyao@foxmail.com>
2017-09-27 00:18:31 +0800

    v1.1
    添加邮件发送模块

--- catch.py
+++ catch.py
@@ -8,6 +8,8 @@ Created on Tue Sep 26 14:28:35 2017
 
 from datetime import datetime
 import urllib
+import os
+os.system("mail.py")
 
 def catch():
      #分解now，并生成PDF链接
@@ -22,12 +24,20 @@ def catch():
     #保存本地
     urllib.urlretrieve(Link06, Ymd + '06.pdf')
     urllib.urlretrieve(Link07,Ymd + '07.pdf')
+    return Ymd+'06.pdf',Ymd+'07.pdf' #返回生成的文件名
 
 print "抓取程序开始运行..."
 now = datetime.now() # 获取当前时间
 baseday = datetime.strptime('2010-12-30', '%Y-%m-%d') #周四基准日
 
 
-if (now - baseday).days %7 == 0: #判断是否为周四，如果是则执行下一步
+if (now - baseday).days %7 != 0: #判断是否为周四，如果是则执行下一步
     print('今天是周四，抓取开始') 
-    catch()
+    name06,name07 = catch() #返回生成文件的文件名
+    print name06,name07
+
+    ret=mail() #函数在mail.py中
+    if ret:
+        print("邮件发送成功")
+    else:
+        print("邮件发送失败")
